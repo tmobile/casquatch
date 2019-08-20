0e49de073fdc61dc5a05f1554c07a9342076ee2c
Josh Turner <turner.josh@gmail.com>
2019-08-20 13:09:57 -0700

    Updated admin scripts for consistency and new release

--- .admin/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Helper script to create cleaned up snapshots
-#
-
-rm casquatch-snapshot.tgz
-tar $(for l in $(cat .gitignore); do echo -n '--exclude '$l' ';done)  -czf casquatch-snapshot.tgz *
